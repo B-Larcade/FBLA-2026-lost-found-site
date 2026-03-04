2b83818d5668e0a8943c9535b43c686fc3bc9318
Braxtyn Larcade <larcade.braxtyn19@gmail.com>
2026-03-04 23:35:26 +0000

    Debug thingys? yeah No clue why this didn't transfer over easily?

--- js/pb-config.js
+++ js/pb-config.js
@@ -1,6 +1,12 @@
 // pb-config.js
 const PB_URL = "https://spheroidal-jimmy-nonrepressible.ngrok-free.dev";
 const pb = new PocketBase(PB_URL);
+//This is for the CORS blocking issues, and why are there so many ways for local profile hosting to fail?
+pb.beforeSend = (url, options) => {
+    options.headers = options.headers || {};
+    options.headers['ngrok-skip-browser-warning'] = 'true';  // bypass ngrok warning page
+    return { url, options };
+};
 
 console.log('PocketBase URL set to:', PB_URL);  // add this temporarily
 
