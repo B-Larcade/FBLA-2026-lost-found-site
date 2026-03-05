9ba84a1cfa423998a08aaac49dc7b89bf765aaf5
Braxtyn Larcade <larcade.braxtyn19@gmail.com>
2026-03-05 01:02:16 +0000

    This is just to fix the navigation for report and index page

--- js/nav.js
+++ js/nav.js
@@ -37,4 +37,21 @@ document.addEventListener('DOMContentLoaded', () => {
             dropdowns.forEach(d => d.classList.remove('open'));
         }
     });
+    
+
+    // This is for when the users are sighned(I'm keeping this mispelling because it's funny) in.
+
+    const loginNavItem = document.getElementById('loginNavItem');
+    const profileDropdown = document.getElementById('profileDropdown');
+    
+    const isLoggedIn = pb?.authStore?.isValid;
+
+    //links visibility for, uh, Profile and Login
+    loginNavItem.style.display = isLoggedIn ? 'none' : 'block';
+    profileDropdown.style.display = isLoggedIn ? 'block' : 'none';
+
+    const profileLink = profileDropdown.querySelector('.dropbtn');
+    if (profileLink) {
+        profileLink.textContent = 'Profile';
+    }
 });
